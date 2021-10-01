81415e9d17a55365ddede993ac59df1bec316e4c
caedonhsieh <caedonhsieh@gmail.com>
2021-09-30 21:47:07 -0500

    try train tracker api call

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
+import React, { useState, useEffect } from 'react';
+
+require('dotenv').config();
+
+// PUT TRAIN_TRACKER_KEY HERE
+const trainTrackerKey = "";
+
 const Header = () => {
-    return(
+    return (
         <h1>CTWay</h1>
     )
 }
 
 const Card = () => {
-    return(
+    return (
         <div className="card">
             <div className="card-body">
                 <div className="card-title">Brynmar</div>
@@ -15,18 +22,33 @@ const Card = () => {
     )
 }
 
+const url = `http://lapi.transitchicago.com/api/1.0/ttfollow.aspx?key=${trainTrackerKey}&runnumber=830&outputType=JSON`;
+
+const getTrainTracker = async (url) => {
+    console.log(trainTrackerKey);
+    console.log(url);
+    const response = await fetch(url, {
+        mode: 'no-cors',
+    });
+    if (!response.ok) throw response;
+    const json = await response.json();
+    console.log(json);
+}
+
 function App() {
-  return (
-    <div>
-        <Header/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-    </div>
-  );
+    useEffect(() => getTrainTracker(url), [])
+
+    return (
+        <div>
+            <Header />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+        </div>
+    );
 }
 
 export default App;
