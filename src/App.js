f8a4a990ea5ae3b956c99fd59521561b24e1ff3c
mitchelllai <mitchelllaimain@gmail.com>
2021-09-30 21:05:08 -0500

    bootstrap template

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+const Header = () => {
+    return(
+        <h1>CTWay</h1>
+    )
+}
+
+const Card = () => {
+    return(
+        <div className="card">
+            <div className="card-body">
+                <div className="card-title">Brynmar</div>
+                <div className="card-text">Red Line</div>
+            </div>
+        </div>
+    )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <Header/>
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
     </div>
   );
 }
