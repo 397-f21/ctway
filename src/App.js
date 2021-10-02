355972d23095456fd2a2437277fa40384e95fc11
d-philip <djphilip@bu.edu>
2021-10-02 10:43:52 -0500

    Added train stops

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from 'react';
 require('dotenv').config();
 
 // PUT TRAIN_TRACKER_KEY HERE
-const trainTrackerKey = "";
+const trainTrackerKey = "344088052b6d4c6a91bccd12775f34b3";
 
 const Header = () => {
     return (
@@ -22,22 +22,31 @@ const Card = () => {
     )
 }
 
-const url = `http://lapi.transitchicago.com/api/1.0/ttfollow.aspx?key=${trainTrackerKey}&runnumber=830&outputType=JSON`;
+const train_url = `http://lapi.transitchicago.com/api/1.0/ttfollow.aspx?key=${trainTrackerKey}&runnumber=830&outputType=JSON`;
+const stops_url = 'https://data.cityofchicago.org/resource/8pix-ypme.json'
+
+const getTrainStops = async (url) => {
+    const response = await fetch(url, {"app_token": "Bekodn643P417LZVWPfAeTldB"});
+    if (!response.ok) throw response;
+    const json = await response.json();
+    console.log(json);
+}
 
 const getTrainTracker = async (url) => {
     console.log(trainTrackerKey);
     console.log(url);
-    const response = await fetch(url, {
-        mode: 'no-cors',
-    });
+    const response = await fetch(url,);
     if (!response.ok) throw response;
     const json = await response.json();
     console.log(json);
 }
 
 function App() {
-    useEffect(() => getTrainTracker(url), [])
-
+    useEffect(() => {
+        getTrainTracker(train_url);
+        getTrainStops(stops_url);
+    }, [])
+    
     return (
         <div>
             <Header />
