898b0e05bf923a570716f06fc1f4f6488936af1a
Katie Li <katie01px2019@sasstudent.org>
2021-10-02 11:13:12 -0500

    add stations

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 
+
 require('dotenv').config();
 
 // PUT TRAIN_TRACKER_KEY HERE
@@ -11,51 +12,56 @@ const Header = () => {
     )
 }
 
-const Card = () => {
+const Card = ({ station }) => {
     return (
         <div className="card">
             <div className="card-body">
-                <div className="card-title">Brynmar</div>
-                <div className="card-text">Red Line</div>
+                <div className="card-title">{station.stop_name}</div>
+                <div className="card-text"></div>
             </div>
         </div>
     )
 }
 
+const StationList = ({ stations }) => (
+    <div>
+        { stations.map(station => <Card key={station.stop_id} station={ station } />) }
+    </div>
+)
+
+
 const train_url = `http://lapi.transitchicago.com/api/1.0/ttfollow.aspx?key=${trainTrackerKey}&runnumber=830&outputType=JSON`;
 const stops_url = 'https://data.cityofchicago.org/resource/8pix-ypme.json'
 
-const getTrainStops = async (url) => {
-    const response = await fetch(url, {"app_token": "Bekodn643P417LZVWPfAeTldB"});
-    if (!response.ok) throw response;
-    const json = await response.json();
-    console.log(json);
-}
 
-const getTrainTracker = async (url) => {
-    console.log(trainTrackerKey);
-    console.log(url);
-    const response = await fetch(url,);
-    if (!response.ok) throw response;
-    const json = await response.json();
-    console.log(json);
-}
+
+// const getTrainTracker = async (url) => {
+//     console.log(trainTrackerKey);
+//     console.log(url);
+//     const response = await fetch(url,);
+//     if (!response.ok) throw response;
+//     const json = await response.json();
+//     console.log(json);
+// }
 
 function App() {
+    const [stations, setStations] = useState([])
     useEffect(() => {
-        getTrainTracker(train_url);
+        const getTrainStops = async (url) => {
+            const response = await fetch(url, {"app_token": "Bekodn643P417LZVWPfAeTldB"});
+            if (!response.ok) throw response;
+            const json = await response.json();
+            console.log(json);
+            setStations(json);
+        }
+        //getTrainTracker(train_url);
         getTrainStops(stops_url);
     }, [])
     
     return (
         <div>
             <Header />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
+            <StationList stations = {stations}/>
         </div>
     );
 }
