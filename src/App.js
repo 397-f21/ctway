df8a9ec98be715d74a0dd784aa3196a644b3a006
caedonhsieh <caedonhsieh@gmail.com>
2021-10-13 00:41:57 -0500

    made filtering

--- src/App.js
+++ src/App.js
@@ -7,9 +7,10 @@ import ReactModal from 'react-modal';
 require('dotenv').config();
 var date = new Date();
 var timestamp = date.getTime();
+const numStations = 10
 // PUT TRAIN_TRACKER_KEY HERE
 const trainTrackerKey = "344088052b6d4c6a91bccd12775f34b3";
-const Header = ({text}) => {
+const Header = ({ text }) => {
     return (
         <div className="header-wrapper">
             <h1>{text}</h1>
@@ -17,7 +18,7 @@ const Header = ({text}) => {
     )
 }
 
-const Card = ({ station, location }) => {
+const Card = ({ station, location, line }) => {
     const formattedLoc = {
         latitude: location.latitude,
         longitude: location.longitude
@@ -38,7 +39,7 @@ const Card = ({ station, location }) => {
                     <div>
                         <div className="icon-style">
                             <svg width="25" height="29" viewBox="0 0 25 29" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M12.5 0.916672C6.66668 0.916672 0.833344 1.64584 0.833344 6.75001V20.6042C0.833344 23.4188 3.12293 25.7083 5.93751 25.7083L3.75001 27.8958V28.625H7.00209L9.91876 25.7083H15.4167L18.3333 28.625H21.25V27.8958L19.0625 25.7083C21.8771 25.7083 24.1667 23.4188 24.1667 20.6042V6.75001C24.1667 1.64584 18.9458 0.916672 12.5 0.916672ZM5.93751 22.7917C4.72709 22.7917 3.75001 21.8146 3.75001 20.6042C3.75001 19.3938 4.72709 18.4167 5.93751 18.4167C7.14793 18.4167 8.12501 19.3938 8.12501 20.6042C8.12501 21.8146 7.14793 22.7917 5.93751 22.7917ZM11.0417 12.5833H3.75001V6.75001H11.0417V12.5833ZM13.9583 12.5833V6.75001H21.25V12.5833H13.9583ZM19.0625 22.7917C17.8521 22.7917 16.875 21.8146 16.875 20.6042C16.875 19.3938 17.8521 18.4167 19.0625 18.4167C20.2729 18.4167 21.25 19.3938 21.25 20.6042C21.25 21.8146 20.2729 22.7917 19.0625 22.7917Z" fill="#542E91" />
+                                <path d="M12.5 0.916672C6.66668 0.916672 0.833344 1.64584 0.833344 6.75001V20.6042C0.833344 23.4188 3.12293 25.7083 5.93751 25.7083L3.75001 27.8958V28.625H7.00209L9.91876 25.7083H15.4167L18.3333 28.625H21.25V27.8958L19.0625 25.7083C21.8771 25.7083 24.1667 23.4188 24.1667 20.6042V6.75001C24.1667 1.64584 18.9458 0.916672 12.5 0.916672ZM5.93751 22.7917C4.72709 22.7917 3.75001 21.8146 3.75001 20.6042C3.75001 19.3938 4.72709 18.4167 5.93751 18.4167C7.14793 18.4167 8.12501 19.3938 8.12501 20.6042C8.12501 21.8146 7.14793 22.7917 5.93751 22.7917ZM11.0417 12.5833H3.75001V6.75001H11.0417V12.5833ZM13.9583 12.5833V6.75001H21.25V12.5833H13.9583ZM19.0625 22.7917C17.8521 22.7917 16.875 21.8146 16.875 20.6042C16.875 19.3938 17.8521 18.4167 19.0625 18.4167C20.2729 18.4167 21.25 19.3938 21.25 20.6042C21.25 21.8146 20.2729 22.7917 19.0625 22.7917Z" fill={Colors[line].hex} />
                             </svg>
                         </div>
                         <div>
@@ -47,22 +48,26 @@ const Card = ({ station, location }) => {
                     </div>
                     <div className="card-text">{roundedDistance} miles</div>
                 </div>
-                
-                {renderArrivals(station.etas)}
-                {/* <a className="card-text" href={directionsURL}>Directions</a> */}
+                {renderArrivals(station.etas[line])}
             </div>
         </div>
     )
 }
 
 const Colors = {
-    'P' : 'Purple'
+    'red': {color: 'red', hex: '#D33D3D'},
+    'org': {color: 'orange', hex: '#EF4925'},
+    'y': {color: 'yellow', hex: '#F5E50B'},
+    'g': {color: 'green', hex: '#109D49'},
+    'blue': {color: 'blue', hex: '#17A0DB'},
+    'brn': {color: 'brown', hex: '#62361C'},
+    'p': {color: 'purple', hex: '#542E91'},
+    'pink': {color: 'pink', hex: '#E37FA7'},
 }
 
 const renderArrivals = (etas) => {
-    console.log(etas)
     return (
-        etas.length > 0 ?
+        etas !== undefined && etas.length > 0 ?
             <div className="content">{etas.map((eta, index) => (
                 <div key={index} className="specific-line">
                     <div>{eta.destNm}</div>
@@ -70,13 +75,21 @@ const renderArrivals = (etas) => {
                 </div>
 
             ))}
-            </div> : null
+            </div> : <div>No trains found</div>
     )
 }
 
-const StationList = ({ stations, location }) => (
+const StationList = ({ stations, location, lines }) => (
     <div className="card-wrapper">
-        {stations.map(station => <Card key={station.map_id} location={location} station={station} />)}
+        {stations.reduce((cards, station) => {
+            const allowedLines = lines.length === 0 ? Object.keys(Colors) : lines
+            for (const line of Object.keys(station.etas)) {
+                if (allowedLines.includes(line) && station.etas[line] !== undefined) {
+                    cards.push(<Card key={station.map_id + line} location={location} station={station} line={line} />);
+                }
+            }
+            return cards
+        }, [])}
     </div>
 )
 
@@ -118,65 +131,74 @@ const formatStations = (ctaData) => {
             location: location,
             station_name: station_name,
             stops: stationMap[map_id],
-            etas: []
+            etas: {}
         }
     });
     return stationList;
 }
 
-const FilterCard = () => (
-    <div>
-        <button onClick = "">Red</button>
-        <button>Orange</button>
-        <button>Yellow</button>
-        <button>Green</button>
-        <button>Blue</button>
-        <button>Purple</button>
-        <button>Pink</button>
-        <button>Brown</button>
+const toggle = (x, lst) => (
+    lst.includes(x) ? lst.filter(y => y !== x) : [x, ...lst]
+);
+
+const FilterCards = ({ selectedLines, setSelectedLines }) => (
+    <div className='filter-card-list'>
+        <FilterCard line='red' selectedLines={selectedLines} setSelectedLines={setSelectedLines} />
+        <FilterCard line='org' selectedLines={selectedLines} setSelectedLines={setSelectedLines} />
+        <FilterCard line='y' selectedLines={selectedLines} setSelectedLines={setSelectedLines} />
+        <FilterCard line='g' selectedLines={selectedLines} setSelectedLines={setSelectedLines} />
+        <FilterCard line='blue' selectedLines={selectedLines} setSelectedLines={setSelectedLines} />
+        <FilterCard line='p' selectedLines={selectedLines} setSelectedLines={setSelectedLines} />
+        <FilterCard line='pink' selectedLines={selectedLines} setSelectedLines={setSelectedLines} />
+        <FilterCard line='brn' selectedLines={selectedLines} setSelectedLines={setSelectedLines} />
     </div>
 )
 
-// const FilterCard = () => (
-//     <div className="modal fade" id="filter" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-//         <div className="modal-dialog">
-//             <div className="modal-content">
-//             <div className="modal-header">
-//                 <h5 className="modal-title" id="exampleModalLabel">Modal title</h5>
-//                 <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-//             </div>
-//             <div className="modal-body">
-//                 ...
-//             </div>
-//             <div className="modal-footer">
-//                 <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-//                 <button type="button" className="btn btn-primary">Save changes</button>
-//             </div>
-//             </div>
-//         </div>
-//     </div>
+const FilterCard = ({ line, selectedLines, setSelectedLines }) => {
+    const isSelected = selectedLines.includes(line);
+    const style = {
+        backgroundColor: isSelected ? 'lightgray' : 'white',
+        marginBottom: '10px',
+        width: '50%',
+        minHeight: '60px',
+    };
 
-// )
+    return (
+        <div className="card"
+            id="card-bootstrap-override"
+            style={style}
+            onClick={() => setSelectedLines(toggle(line, selectedLines))}>
+            <div className="card-body">
+                <div className="card-title">{Colors[line].color}</div>
+                <div className="icon-style">
+                    <svg width="25" height="29" viewBox="0 0 25 29" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M12.5 0.916672C6.66668 0.916672 0.833344 1.64584 0.833344 6.75001V20.6042C0.833344 23.4188 3.12293 25.7083 5.93751 25.7083L3.75001 27.8958V28.625H7.00209L9.91876 25.7083H15.4167L18.3333 28.625H21.25V27.8958L19.0625 25.7083C21.8771 25.7083 24.1667 23.4188 24.1667 20.6042V6.75001C24.1667 1.64584 18.9458 0.916672 12.5 0.916672ZM5.93751 22.7917C4.72709 22.7917 3.75001 21.8146 3.75001 20.6042C3.75001 19.3938 4.72709 18.4167 5.93751 18.4167C7.14793 18.4167 8.12501 19.3938 8.12501 20.6042C8.12501 21.8146 7.14793 22.7917 5.93751 22.7917ZM11.0417 12.5833H3.75001V6.75001H11.0417V12.5833ZM13.9583 12.5833V6.75001H21.25V12.5833H13.9583ZM19.0625 22.7917C17.8521 22.7917 16.875 21.8146 16.875 20.6042C16.875 19.3938 17.8521 18.4167 19.0625 18.4167C20.2729 18.4167 21.25 19.3938 21.25 20.6042C21.25 21.8146 20.2729 22.7917 19.0625 22.7917Z" fill={Colors[line].hex} />
+                    </svg>
+                </div>
+            </div>
+        </div>
+    );
+};
 
-function disPlayMin(timeStamp){
+function disPlayMin(timeStamp) {
 
-    var minTimeStamp = Math.floor((timeStamp)/60000);
+    var minTimeStamp = Math.floor((timeStamp) / 60000);
 
-    if (minTimeStamp < 0){
+    if (minTimeStamp < 0) {
         return "Just Arrived";
     }
-    else if(minTimeStamp <= 1){
+    else if (minTimeStamp <= 1) {
         return "Arriving Now";
     }
-    else{
+    else {
         return minTimeStamp + " mins";
     }
 }
 
-function toTimestamp(strDate){
+function toTimestamp(strDate) {
     var datum = Date.parse(strDate);
-    return datum/1000;
- }
+    return datum / 1000;
+}
 //  alert(toTimestamp('02/13/2009 23:31:30'));
 
 function App() {
@@ -204,6 +226,7 @@ function App() {
     }
 
     const closeModal = () => {
+        updateNearStations(stations, selectedLines, numStations)
         setModalVisible(false);
     }
 
@@ -215,34 +238,44 @@ function App() {
         setStations(formattedStations);
     }
 
+    const stationHasLine = (station, line) => {
+        for (const stop of station.stops) {
+            if (stop[line] === true) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     const updateNearStations = async (stations, lines, k) => {
-
-        
-
         const filtered_stations = lines.length ? stations.filter((station) => {
-            for (const stop of station.stops) {
-                for (const line of lines) {
-                    if (stop[line] === true) {
-                        return true;
-                    }
+            for (const line of lines) {
+                if (stationHasLine(station, line)) {
+                    return true;
                 }
             }
             return false;
         }) : stations;
         const kNearest = kNearestStations(filtered_stations, userLoc, k);
-        updateEtas(kNearest);
+        updateEtas(kNearest, lines);
     }
 
     const kNearestStations = (stations, userLoc, k) =>
         sortByDist(stations, userLoc).slice(0, k);
 
-    const updateEtas = async (stations) => {
+    const updateEtas = async (stations, lines) => {
+        const allowedLines = lines.length === 0 ? Object.keys(Colors) : lines
         for (let i = 0; i < stations.length; i++) {
-            const train_url = `https://cors-anywhere.herokuapp.com/http://lapi.transitchicago.com/api/1.0/ttarrivals.aspx?key=${trainTrackerKey}&mapid=${stations[i].map_id}&max=3&outputType=JSON`;
-            const response = await fetch(train_url);
-            if (!response.ok) throw response;
-            const json = await response.json();
-            stations[i].etas = json.ctatt.eta;
+            for (const line of allowedLines) {
+                if (stationHasLine(stations[i], line)) {
+                    const train_url = `https://cors-anywhere.herokuapp.com/http://lapi.transitchicago.com/api/1.0/ttarrivals.aspx?key=${trainTrackerKey}&mapid=${stations[i].map_id}&rt=${line}&max=3&outputType=JSON`;
+                    const response = await fetch(train_url);
+                    if (!response.ok) throw response;
+                    const json = await response.json();
+                    stations[i].etas[line] = json.ctatt.eta;
+                }
+            }
+            
         }
         setNearStations(stations);
     }
@@ -252,7 +285,7 @@ function App() {
         getTrainStops(stops_url);
     }, [])
 
-    if (stations.length === 0 || !userLoc){
+    if (stations.length === 0 || !userLoc) {
         return (
             <div className="loading">
                 <h1>Loading...</h1>
@@ -261,17 +294,17 @@ function App() {
     };
     // if (!userLoc) return <h1>Awaiting user location...</h1>;
 
-    if (buttonClicked === false){
-        return(
+    if (buttonClicked === false) {
+        return (
             <div className="app-wrapper">
-                <Header text={"Find Nearby Stations"}/>
-                <div className = "button-wrapper">
+                <Header text={"Find Nearby Stations"} />
+                <div className="button-wrapper">
                     <div className="button-style" onClick={() => {
-                        updateNearStations(stations, 1);
+                        updateNearStations(stations, selectedLines, numStations);
                         setbuttonClicked(true);
                     }}>
                         <svg width="63" height="79" viewBox="0 0 63 79" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M31.25 0L0 76.2083L2.95833 79.1667L31.25 66.6667L59.5417 79.1667L62.5 76.2083L31.25 0Z" fill="white"/>
+                            <path d="M31.25 0L0 76.2083L2.95833 79.1667L31.25 66.6667L59.5417 79.1667L62.5 76.2083L31.25 0Z" fill="white" />
                         </svg>
                         <h1>Current Location</h1>
                     </div>
@@ -281,13 +314,13 @@ function App() {
     }
     return (
         <div className="app-wrapper">
-            <Header text={"Nearest Stations"}/>
+            <Header text={"Nearest Stations"} />
             <FilterButton />
-            <StationList location={userLoc} stations={nearStations} />
-            {/* <FilterCard /> */}
+            <StationList location={userLoc} stations={nearStations} lines={selectedLines}/>
             <ReactModal isOpen={modalVisible} onRequestClose={closeModal}>
-                <p>modal here</p>
-
+                <div className="modal-text">Filter by Line</div>
+                <FilterCards selectedLines={selectedLines} setSelectedLines={setSelectedLines} />
+                <button className="btn btn-primary" onClick={closeModal}>Close</button>
             </ReactModal>
         </div>
     );
