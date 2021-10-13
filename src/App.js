1a263f62b9a8d6c880187d93f968cf23934454c3
d-philip <djphilip@bu.edu>
2021-10-12 21:05:16 -0500

    Added filter

--- src/App.js
+++ src/App.js
@@ -123,6 +123,30 @@ const formatStations = (ctaData) => {
     return stationList;
 }
 
+const FilterButton = () => (
+    <button className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#filter">Filter</button>
+)
+
+const FilterCard = () => (
+    <div className="modal fade" id="filter" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div className="modal-dialog">
+            <div className="modal-content">
+            <div className="modal-header">
+                <h5 className="modal-title" id="exampleModalLabel">Modal title</h5>
+                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div className="modal-body">
+                ...
+            </div>
+            <div className="modal-footer">
+                <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="button" className="btn btn-primary">Save changes</button>
+            </div>
+            </div>
+        </div>
+    </div>
+
+)
 
 function disPlayMin(timeStamp){
 
@@ -219,7 +243,9 @@ function App() {
     return (
         <div className="app-wrapper">
             <Header text={"Nearest Stations"}/>
+            <FilterButton />
             <StationList location={userLoc} stations={nearStations} />
+            <FilterCard />
         </div>
     );
 }
